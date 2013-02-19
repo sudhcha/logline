121351f1267569dc3ff522b41f251cf63e5aa25e
vijayrc <vijayrc@outlook.com>
2013-02-19 10:19:39 -0500

    context add logic works

--- src/main/java/com/vrc/logline/rule/KeyRule.java
+++ src/main/java/com/vrc/logline/rule/KeyRule.java
@@ -11,26 +11,38 @@ import java.util.regex.Pattern;
 public class KeyRule extends BaseRule {
     private Pattern threadPattern;
     private Pattern keyPattern;
+    private Integer contextSize;
 
     public KeyRule(List<String> keys) {
         threadPattern = Pattern.compile("[0-9]{4}-[0-9]{2}-[0-9]{2}\\s*[0-9]{1,2}:[0-9]{2}:[0-9]{2},[0-9]{0,3}\\s*(?<thread>.*)\\s*(INFO|ERROR)");
         for (int i = 0; i < keys.size(); i++)
             keys.set(i, StringUtils.trimToEmpty(keys.get(i)));
         keyPattern = Pattern.compile("(?i)" + StringUtils.join(keys.toArray(), "|"));
+        this.contextSize = 20;
     }
 
     @Override
     public void apply(String file, List<String> inputLines, Set<Line> outputLines, RulePackage rulePackage) {
-        for (String inputLine : inputLines) {
+        for (int i = 0; i < inputLines.size(); i++) {
+            String inputLine = inputLines.get(i);
             if (!keyPattern.matcher(inputLine).find())
                 continue;
+
             Matcher matcher = threadPattern.matcher(inputLine);
             if (matcher != null && matcher.find()) {
                 String thread = matcher.group("thread");
                 rulePackage.addThread(thread);
                 outputLines.add(new Line(inputLine).ofFile(file).ofThread(thread));
+                for (int j = i + contextSize; j != i && j < inputLines.size(); j--)
+                    outputLines.add(new Line(inputLines.get(j)).ofFile(file).ofThread(thread));
+                for (int k = i - contextSize; k != i && k < 0; k++)
+                    outputLines.add(new Line(inputLines.get(k)).ofFile(file).ofThread(thread));
             } else {
                 outputLines.add(new Line(inputLine).ofFile(file));
+                for (int j = i + contextSize; j != i && j < inputLines.size(); j--)
+                    outputLines.add(new Line(inputLines.get(j)).ofFile(file));
+                for (int k = i - contextSize; k != i && k < 0; k++)
+                    outputLines.add(new Line(inputLines.get(k)).ofFile(file));
             }
         }
     }
